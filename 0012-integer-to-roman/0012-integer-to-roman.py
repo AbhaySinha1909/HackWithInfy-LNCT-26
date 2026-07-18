5e9983e9d58c0fdc76b817486ca9def3c1c76321
Abhay Sinha <161612516+AbhaySinha1909@users.noreply.github.com>
2026-07-18 17:27:57 +0530

    Time: 3 ms (77.42%), Space: 19.3 MB (64.58%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,18 +1,9 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        n = len(str(num))
-        my_dict = {3000 : "MMM", 2000 : "MM", 1000 : "M", 
-                    900:"CM", 800:"DCCC", 700 : "DCC", 600 : "DC", 500 : "D",
-                    400 : "CD", 300 : "CCC", 200 : "CC", 100 : "C",
-                    90 : "XC", 80 : "LXXX", 70 : "LXX", 60 : "LX", 50 : "L",
-                    40 : "XL", 30 : "XXX", 20 : "XX", 10 : "X",
-                    9 : "IX", 8 : "VIII", 7 : "VII", 6:"VI", 5:"V", 
-                    4:"IV", 3:"III", 2:"II", 1:"I" }
-        p = ""
-        
-        for key in sorted(my_dict.keys(), reverse=True):
-            while num >= key:
-                p += my_dict[key]
-                num -= key
-
-        return p
+        th = ["", "M", "MM", "MMM"]
+        hd = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        tt = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        on = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    
+        P = (th[num//1000] + hd[(num%1000)//100] + tt[(num%100)//10] + on[num%10])
+        return P
