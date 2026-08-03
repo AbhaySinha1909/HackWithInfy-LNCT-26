7f9e45f4a9d0c9dc4e31ee5807ed58e78e73c5a3
Abhay Sinha <161612516+AbhaySinha1909@users.noreply.github.com>
2026-08-03 10:16:36 +0530

    Time: 1 ms (81.95%), Space: 19.4 MB (26.07%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,5 +1,13 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        import math
-        a = int((math.sqrt(1+8*n)-1)//2)
-        return a
+        low, high = 0, n
+        while low <= high:
+            mid = (low + high) // 2
+            coin = mid * (mid + 1) // 2 
+            if coin == n :
+                return mid
+            elif coin < n:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return high
